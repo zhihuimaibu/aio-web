fd63ded31ed62a38508339218eb81dfb4bffa03f
zhihuimaibu <45163390+zhihuimaibu@users.noreply.github.com>
2024-02-19 18:54:47 +0800

    Update month-balance.vue
    收支

--- src/components/month/month-balance.vue
+++ src/components/month/month-balance.vue
@@ -3,53 +3,109 @@
     <div class="buttons">
       <el-button @click="handleAddRow">新增行</el-button>
       <el-button @click="handleAddColumn">新增列</el-button>
+      <el-button @click="handleSave">保存</el-button>
     </div>
     <div class="table">
-      <el-table border :data="tableData" style="height: 100%;">
-        <el-table-column v-for="c in tableColumns" :key="c.id" :prop="c.columnName" :label="c.columnName">
+      <el-table 
+        border 
+        :data="tableData" 
+        style="height: 100%;">
+        <el-table-column width="40">
+          <template #default="scope">
+            <el-icon @click="handleDeleteRow(scope.row)"><Delete /></el-icon>
+          </template>
+        </el-table-column>
+        <el-table-column 
+          v-for="c in tableColumns" 
+          :key="c.cid"
+          :prop="c.columnName"
+          :label="c.columnName"
+          :class-name="c.selected ? 'selected-cell' : ''">
           <template #header="scope">
-            <el-input 
-              v-model="c.columnName" 
-              @update:modelValue="handleInput"/>
+            <div class="header" 
+              @mouseenter="handleDeleteColumn(c)"
+              @mouseleave="handleLeaveColumn">
+              <el-icon><Delete /></el-icon>
+              <el-input
+                v-model="c.columnName" 
+                @input="handleInput(c)"
+                @focus="handleInput(c)"/>
+            </div>
           </template>
           <template #default="scope">
             <el-input v-model="scope.row[c.columnName]"/>
-      </template>
-      </el-table-column>
+          </template>
+        </el-table-column>
       </el-table>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+  import { ref, watch } from 'vue'
+  import { Delete } from '@element-plus/icons-vue'
 
-  let id = 0
-
-  const tableData = ref([{}])
+  let cid = 0
+  let rid = 0
+  let curColumn = ref('')
+  let curCid = ref('')
+  let oldCid 
+  const tableData = ref([{
+    rid,
+  }])
   const tableColumns = ref([{
-    id
+    cid,
+    selected: false
   }])
 
-  const handleInput = function() {
-    tableColumns.value.forEach(c => {
-      tableData.value = tableData.value.map((d) => {
-        d[c.columnName] = d[c.columnName] ? d[c.columnName] : ''
-        return d
-      })
+  const handleDeleteRow = function(row) {
+    console.log(row);
+  }
+  
+  const handleLeaveColumn = function() {
+    tableColumns.value.map(tc => {
+      tc.selected = false
+      return tc
     })
+  }
+
+  const handleDeleteColumn = function(c) {
+    tableColumns.value.map(tc => {
+      if (tc.cid === c.cid) {
+        tc.selected = !tc.selected
+      }else {
+        tc.selected = false
+      }
+      return tc
+    })
+    console.log(c);
+  }
+
+  watch(curColumn, (newValue, oldValue) => {
+    if (oldCid === curCid.value) {
+      tableData.value.forEach(td => {
+        td[newValue] = td[oldValue]
+        delete td[oldValue]
+      })
+    }
+
+    oldCid = curCid.value
+  })
+
+  const handleInput = function(c) {
+    curColumn.value = c.columnName
+    curCid.value = c.cid
   }
 
   const handleAddRow = function() {
     tableData.value.push({
-      
+      rid: ++rid
     })
   }
 
   const handleAddColumn = function() {
     tableColumns.value.push({
-      columnName: '',
-      id: ++id
+      cid: ++cid
     })
   }
 </script>
@@ -66,5 +122,11 @@ import { ref } from 'vue'
     padding-top: 1rem;
     flex: 1;
     overflow: hidden;
+  }
+  .header {
+    text-align: center;
+  }
+  .table >>> .selected-cell {
+    background: #f5f7fa;
   }
 </style>
